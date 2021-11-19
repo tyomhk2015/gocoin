144ddb8f9b54a29f4265e6cd2bcd7d7d8e6b6484
tyomhk <cheertrain15@naver.com>
2021-11-20 00:06:36 +0900

    More notes on blockchain theory.

---
+++ person/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/tyomhk2015/gocoin/person/person"
+)
+
+func main() {
+	vtuber := person.Person{}
+	vtuber.Set("Kiryu", 999)
+	fmt.Println("This talent is", vtuber)
+}
+
+func sing(shiny, smile, story string) {
+	fmt.Println(shiny, smile, story)
+}
+
+type talent struct {
+	name     string
+	language string
+}
+
+func (t talent) stream(time int) {
+	fmt.Printf("%s's stream starts at %dpm, %s.", t.name, time, t.language)
+}
