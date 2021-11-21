8a031b947ab1933ebb490f4cd2b91ecc8390e7c5
tyomhk <cheertrain15@naver.com>
2021-11-22 01:10:29 +0900

    Added main.go file of blockchain code, and fixed the directory issue of blockchain.go file.

---
+++ blockchain/main.go
@@ -0,0 +1,13 @@
+package blockchain
+
+import (
+	"github.com/tyomhk2015/gocoin/blockchain/blockchain"
+)
+
+func main() {
+	myChain := blockchain.GetBlockchain()
+	myChain.AddBlock("Cover")
+	myChain.AddBlock("Hololive")
+	myChain.AddBlock("0-Gen-Members")
+	myChain.ShowAllBlocks()
+}
