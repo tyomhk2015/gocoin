c7712509a8a50bc7e374e25e35654fbbc86de9d6
tyomhk <cheertrain15@naver.com>
2021-11-28 00:16:51 +0900

    Made backup of past blockchain code, for bolt DB.

---
+++ blockchain_origin/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/tyomhk2015/gocoin/blockchain/blockchain"
+)
+
+func main() {
+	myChain := blockchain.GetBlockchain()
+	myChain.AddBlock("Cover")
+	myChain.AddBlock("Hololive")
+	myChain.AddBlock("0-Gen-Members")
+	myChain.ShowAllBlocks()
+}
